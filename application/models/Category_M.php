0bed8cb3283d02d9e11019df0a4978dfca9a1773
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-08 14:39:11 +0700

    Create Category_M.php

---
+++ application/models/Category_M.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_M extends MY_Model {
+	protected $table = 'db_category';
+}
+
+/* End of file Category_M.php */
+/* Location: ./application/models/Category_M.php */
