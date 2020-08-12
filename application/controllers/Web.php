9adf3f7a9bf5ba6ee4735fb8303cd4cdef80c32f
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-12 12:30:32 +0700

    trang khu dân cư list

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -8,7 +8,7 @@ class Web extends MY_Controller {
         parent::__construct();
     }
     public function index($alias=null){
-        $this->page_project_detail();
+        $this->page_khudancu_all();
     }
 
     public function page_index($alias=null){
@@ -24,6 +24,11 @@ class Web extends MY_Controller {
     public function page_khudancu_detail($alias=null){
         $this->page_header();
         $this->view('web/khudancu-detail');
+        $this->page_footer();
+    }
+    public function page_khudancu_all($alias=null){
+        $this->page_header();
+        $this->view('web/khudancu-all');
         $this->page_footer();
     }
     public function page_project_detail($alias=null){
---
+++ application/views/web/khudancu-all.php
@@ -0,0 +1,30 @@
+<section class="page-khudancu-detail">
+    <div class="banner" style="background:url(<?=base_url('upload/images/kdc_bg.jpg')?>)">
+        <div class="abs-content container">
+            <div class="col-md-12 pd0">
+                <p>Khu dân cư  TP. Hồ Chí Minh  Quận 2</p>
+                <h3 class="title">Khu đô thị mới Thủ Thiêm</h3>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <?php for ($i=0;$i<10;$i++){ ?>
+                <div class="col-md-4 col-xs-6">
+                        <div class="item-khudancu-ll">
+                            <a href="">
+                                <div class="img-cover">
+                                    <img src="<?=base_url('upload/images/image.jpg')?>" alt="">
+                                </div>
+                                <div class="content">
+                                    <h4 class="title">Tân thuận tây</h4>
+                                    <p>Quận 7, TP. Hồ Chí Minh</p>
+                                </div>
+                            </a>
+                        </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</section>
