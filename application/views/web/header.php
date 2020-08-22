e9ef767f5d0efa19b0d952ce8bf35d5b517d6aa4
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-22 13:22:27 +0700

    Update header.php

--- application/views/web/header.php
+++ application/views/web/header.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$title?></title>
-    <meta property="og:url"                content="<?=fullAddtess()?>" />
+    <meta property="og:url"                content="<?=fullAddress()?>" />
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="<?=$title?>" />
     <meta property="og:description"        content="<?=$description?>" />
