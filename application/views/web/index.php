c8b4d1d3aab9ae98a4249de1c1a6da7305597982
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-23 21:46:40 +0700

    Update index.php

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -48,7 +48,7 @@
                 </div>
             <?php } ?>
             <div class="col-md-12">
-                <div><a href="">Xem thêm dự án <span class="fa fa-angle-right"></span></a></div>
+                <br><a href="">Xem thêm dự án <span class="fa fa-angle-right"></span></a>
             </div>
         </div>
     </div>
@@ -88,7 +88,7 @@
                 $col = 4; include ('duan-item.php');
              ?>
             <div class="col-md-12">
-               <div> <a href="">Xem thêm nhà đất <span class="fa fa-angle-right"></span></a></div>
+               <br><a href="">Xem thêm nhà đất <span class="fa fa-angle-right"></span></a>
             </div>
         </div>
     </div>
@@ -127,7 +127,7 @@
                 $col = 4; include ('duan-item.php');
              ?>
             <div class="col-md-12">
-                <div><a href="">Xem thêm nhà đất <span class="fa fa-angle-right"></span></a></div>
+                <a href="">Xem thêm nhà đất <span class="fa fa-angle-right"></span></a>
             </div>
         </div>
     </div>
@@ -173,7 +173,7 @@
                 </div>
             <?php } ?>
             <div class="col-md-12">
-                <div><br><a href="<?=base_url('chu-dau-tu')?>">Xem thêm chủ đầu tư <span class="fa fa-angle-right"></span></a></div>
+                <br><a href="<?=base_url('chu-dau-tu')?>">Xem thêm chủ đầu tư <span class="fa fa-angle-right"></span></a>
             </div>
         </div>
     </div>
