6f0d3ee5591a3ad8bbe65887dbfa10d1285e0951
hứa văn dũng <60870815+dunghuava@users.noreply.github.com>
2020-08-28 22:26:47 +0700

    Update form-contact.php

--- application/views/web/form-contact.php
+++ application/views/web/form-contact.php
@@ -10,7 +10,7 @@
         padding:5px;
     }
 </style>
-<br><br>
+<div class="hidden-md hidden-lg"><br><br></div>
 <div class="form-mobile hidden-md hidden-lg">
     <div style="display:flex">
         <div class="col-md-6 col-xs-6 div-contact-tel" style="margin:2px">
