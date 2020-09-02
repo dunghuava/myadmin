58bd942dda77f536a968ae7c209ec715204c25c1
TruongThuan1997 <34022612+TruongThuan1997@users.noreply.github.com>
2020-09-03 00:06:55 +0700

    fix address

--- application/views/web/duan-item-h.php
+++ application/views/web/duan-item-h.php
@@ -21,7 +21,7 @@
                                 <?=$item['project_title']?>
                             </a>
                         </h3>
-                        <p class="text-overflow"><span class="fa fa-map-marker"></span> <b>P.&nbsp;<?=$info_ward['ward_name'].', '.$info_district['district_name'].', '.$info_province['province_name']?></b></p>
+                        <p class="text-overflow"><span class="fa fa-map-marker"></span> <b><?=$item['project_address']?></b></p>
                         <p style="display:inline-block;color:red" class="price">Giá bán:&nbsp;<?=$item['project_price']?></p>
                         <?php if ($item['project_price_lease']>0){ ?>
                             <p style="display:inline-block;color:red" class="price right">Giá thuê: <?=$item['project_price_lease']?></p>
